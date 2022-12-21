3ab69a92e4da39fa21992cb12b5a00098bfc8b14
Ivan <iv@Ivans-MacBook-Air.local>
2022-12-21 13:47:17 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { MainLayout } from "../layouts/MainLayout";
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
@@ -7,7 +8,12 @@ export default function Home() {
       metaDescription={"Alex Villas"}
       metaKeywords={"alex villas"}
     >
-      <div className={"text-5xl text-amber-600"}>Контент для главной</div>
+      <Hero
+        h1first={"Alex Villas"}
+        h1second={"Agency"}
+        text={"High-performing \nproperty agency \nin prime locations of Bali"}
+        subLable={"STARTING FROM $189.000"}
+      />
     </MainLayout>
   );
 }
