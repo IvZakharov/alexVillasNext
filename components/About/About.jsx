608b2fd1ccb424bdcc9ece6c6cf6c95488f16d7c
Ivan <iv@Ivans-MacBook-Air.local>
2023-02-15 16:09:55 +0300

    fix about grid

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -87,45 +87,55 @@ const About = () => {
   ];
   return (
     <section className={styles.about}>
-      <div className={"container xl:pl-32 xl:pt-32 xl:pb-32"}>
-        <div className={`${styles.videoPlaceholder}`}>
-          <Image
-            alt="video placeholder"
-            src={"/images/about/placeholder.jpg"}
-            quality={80}
-            fill
-            sizes="100vw"
-            style={{
-              objectFit: "cover",
-            }}
-          />
-          <button className={styles.playBtn} onClick={openModal}>
-            <Image src={playSvg} alt={"playSvg"} />
-          </button>
-        </div>
-
-        <div className={"grid md:grid-cols-2 xl:grid-cols-3 relative z-10 "}>
-          <div className={""}>
-            <p className={"text  mb-5 md:mb-8"}>
-              ALEX VILLAS - это группа компаний на рынке недвижимости Бали.
-            </p>
-            <p className={"text mb-5 md:mb-8"}>
-              8 лет опыта, 3500 отзывов на AirBNB, 78 инвесторов и 176 объектов
-              в работе.
-            </p>
+      <div className={"container"}>
+        <div className={"xl:pt-32 xl:pb-32"}>
+          <div className={`${styles.videoPlaceholder}`}>
+            <Image
+              alt="video placeholder"
+              src={"/images/about/placeholder.jpg"}
+              quality={80}
+              fill
+              sizes="100vw"
+              style={{
+                objectFit: "cover",
+              }}
+            />
+            <button className={styles.playBtn} onClick={openModal}>
+              <Image src={playSvg} alt={"playSvg"} />
+            </button>
           </div>
-        </div>
-        <div className={"grid grid-cols-1 xl:grid-cols-3 relative z-10"}>
-          <nav className={styles.nav}>
-            <ul className={"grid grid-cols-1 sm:grid-cols-2 gap-2.5"}>
-              {linksRu &&
-                linksRu.map((link, idx) => (
-                  <li key={idx}>
-                    <Link href={link.link}>{link.label}</Link>
-                  </li>
-                ))}
-            </ul>
-          </nav>
+
+          <div
+            className={
+              "grid md:grid-cols-2 xl:grid-cols-3 relative z-10 transform: xl:translate-x-32"
+            }
+          >
+            <div>
+              <p className={"text  mb-5 md:mb-8"}>
+                ALEX VILLAS - это группа компаний на рынке недвижимости Бали.
+              </p>
+              <p className={"text mb-5 md:mb-8"}>
+                8 лет опыта, 3500 отзывов на AirBNB, 78 инвесторов и 176
+                объектов в работе.
+              </p>
+            </div>
+          </div>
+          <div
+            className={
+              "grid grid-cols-1 xl:grid-cols-12 relative z-10 transform: xl:translate-x-32"
+            }
+          >
+            <nav className={`${styles.nav} xl:col-span-4`}>
+              <ul className={"grid grid-cols-1 sm:grid-cols-2 gap-2.5"}>
+                {linksRu &&
+                  linksRu.map((link, idx) => (
+                    <li key={idx}>
+                      <Link href={link.link}>{link.label}</Link>
+                    </li>
+                  ))}
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
 
