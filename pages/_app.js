e83aea27012915de343d1058c1c8d2ef319381d6
Ivan <iv@Ivans-MacBook-Air.local>
2023-03-01 10:15:55 +0300

    add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/globals.scss";
+import NextNProgress from 'nextjs-progressbar';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress color="rgba(255,153,0, 0.8)" startPosition={0.3} stopDelayMs={200} height={0} showOnShallow={true} />
+  
+  
       <Component {...pageProps} />
     </>
   );
